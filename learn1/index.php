9c1e94775a0d7899f8208ddb5043cc0cc056aaef
arif cahyady <aripcahyadi140@gmail.com>
2022-05-23 12:40:13 +0700

    Learn PHP Intermediate 1

---
+++ learn1/index.php
@@ -0,0 +1,64 @@
+<?php  
+// Learn 2 - PHP
+// Sintaks PHP
+
+// Standar Output
+// echo, print
+// echo "Arif Cahyady";
+// print " This PHP";
+// print_r
+// print_r("PHP Awesome");
+// var_dump
+// var_dump("Example PHP");
+
+
+// Penulisan Sintaks PHP
+// 1.HTML in PHP
+// echo "This is Example -> <br>";
+// 2.PHP in HTML
+
+
+// Variabel
+// $name = "Arif Cahyady";
+// echo $name;
+
+
+// Operator
+// Aritmatika
+// +, -, /, %, =
+// $x = 10;
+// $y = 20;
+// echo $x + $y;
+
+// Concatenation
+// . 
+// $nama_depan = "Arif";
+// $nama_belakang = "Cahyady";
+// echo $nama_depan . $nama_belakang;
+
+// Assignment
+// +=, -=, /=, %=, .=
+// $x = 10;
+// $x += 12;
+// echo $x;
+
+// Perbandingan
+// <, >, <=, >=, ==, !=
+// $x = 10;
+// var_dump($x <= 20);
+
+
+// Identitas
+// ===, !==
+// $x = 20;
+// var_dump($x === "20")
+
+
+// Logika
+// &&, ||, !
+// $x = 20;
+// var_dump($x > 30 && $x % 2 == 0)
+// var_dump($x > 30 || $x % 2 == 0)
+
+
+?>
